d9cac9c26765931236230b1200c0036a0e74b865
BleedCopper <rissamquindoza@gmail.com>
2024-11-08 02:30:32 +1300

    adding prometheus

--- compute/express/index.js
+++ compute/express/index.js
@@ -2,11 +2,22 @@ import cors from "cors";
 import dotenv from "dotenv";
 import express from "express";
 import mongoose from "mongoose";
+import promClient from "prom-client";
 import viewCountRoutes from "./routes/viewCount.js";
 
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
+
+const register = new promClient.Registry();
+
+// Create a simple histogram metric for HTTP request durations
+const httpRequestDurationMicroseconds = new promClient.Histogram({
+  name: "http_request_duration_seconds",
+  help: "Duration of HTTP requests in seconds",
+  buckets: [0.1, 0.2, 0.5, 1, 2, 5], // Buckets for response times
+});
+register.registerMetric(httpRequestDurationMicroseconds);
 
 // Middleware
 app.use(cors());
@@ -23,6 +34,17 @@ mongoose
   });
 
 app.use("/api", viewCountRoutes);
+
+// Define the /metrics endpoint for Prometheus to scrape
+app.get("/metrics", async (req, res) => {
+  res.set("Content-Type", register.contentType);
+  res.end(await register.metrics());
+});
+
+// Sample route
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
 
 // Start server
 app.listen(PORT, () => {
